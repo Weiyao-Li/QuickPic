9dc845357dec6c6b2500acdf15dfc4d3b506f0e0
WeiyaoLi <nolanwyl@gmail.com>
2023-03-18 15:46:56 -0400

    LF2 update, needs modification

--- LF2.py
+++ LF2.py
@@ -1,79 +1,106 @@
-import os
 import json
+import os
 import boto3
+import time
+import logging
+import re
+import datetime
+import requests
 from opensearchpy import OpenSearch, RequestsHttpConnection
-from opensearchpy.helpers import to_dict
-from botocore.exceptions import ClientError
 from requests_aws4auth import AWS4Auth
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-REGION = "us-east-1"
-LEX_RUNTIME = boto3.client('lexv2-runtime', region_name=REGION)
-OPENSEARCH_ENDPOINT = "search-photos-eumyc7ppo2p53eh3lnadp7jyqq.us-east-1.es.amazonaws.com"
-OPENSEARCH_INDEX = "photos"
+region = "us-east-1"
+host = "search-photos-eumyc7ppo2p53eh3lnadp7jyqq.us-east-1.es.amazonaws.com"
+index = "photos"
 
 lexv2 = boto3.client("lexv2-runtime")
-# Set up OpenSearch client
-def get_awsauth(REGION, service):
-    cred = boto3.Session().get_credentials()
-    return AWS4Auth(cred.access_key,
-                    cred.secret_key,
-                    REGION,
-                    service,
-                    session_token=cred.token)
-
-
-
 def lambda_handler(event, context):
-    query = event["queryString"]
 
-
-
-    # Disambiguate the query using Amazon Lex V2 bot
-
+    msg_from_user = 'I need some photos with trees'
     response = lexv2.recognize_text(
         botId='5GMXHBNSCD',
         botAliasId='TSTALIASID',
         localeId='en_US',
         sessionId='testuser',
-        text=query
+        text=msg_from_user
     )
-    print(response)
-    # If Amazon Lex V2 disambiguation request yields any keywords
+
     keywords = []
     if 'interpretations' in response and len(response['interpretations']) > 0:
         interpretation = response['interpretations'][0]
-        if 'slots' in interpretation:
-            for slot in interpretation['slots']:
-                if slot['value']['interpretedValue']:
-                    keywords.append(slot['value']['interpretedValue'])
+        if 'slots' in interpretation["intent"]:
+            for key, value in interpretation["intent"]["slots"].items():
+                if key in ["query_term1", "query_term2", "query_term3"] and value:
+                    key_words = value["value"]["interpretedValue"]
+                    if " " in key_words:
+                        key_words = key_words.split(" ")
+                        for word in key_words:
+                            keywords.append(word)
+                    else:
+                        keywords.append(key_words)
 
-    # Search the OpenSearch index for results and return them accordingly
+    print(keywords)
     if keywords:
-        search_results = search_photos(keywords)
-        return {"results": search_results}
+        img_paths = search_photos(keywords)
+
+    if not img_paths:
+        return {
+            'statusCode': 200,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            'body': json.dumps('No Results found')
+        }
     else:
-        return {"results": []}
+        return {
+            'statusCode': 200,
+            'headers': {"Access-Control-Allow-Origin": "*"},
+            'body': {
+                'imagePaths': img_paths,
+                'userQuery': q1,
+                'labels': labels,
+            }
+        }
+
 
 def search_photos(keywords):
-    if len(keywords)>1:
-        term = " ".join(keywords)
-        q = {'size': 20, 'query': {'multi_match': {'query': term}}}
-    else:
-        q =  {'size': 20, 'query': {'multi_match': {'query': keywords[0]}}}
+    query = {
+        "query": {
+            "bool": {
+                "should": []
+            }
+        }
+    }
+
+    for key in keywords:
+        query['query']['bool']['should'].append({
+            "match": {
+                "labels": key
+            }
+        })
+
+    opensearch = OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=get_awsauth(region, "es"),
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
 
     try:
-        opensearch = OpenSearch(
-            hosts=[{'host': OPENSEARCH_ENDPOINT, 'port': 443}],
-            http_auth=get_awsauth(REGION, "es"),
-            use_ssl=True,
-            verify_certs=True,
-            connection_class=RequestsHttpConnection
-        )
-        response = opensearch.search(index=OPENSEARCH_INDEX, body=q)
+        response = opensearch.search(body=query, index="photos")
+        print(response)
+        hits = response['hits']['hits']
+        return hits
+
     except Exception as e:
         print(f"Error while searching OpenSearch index: {str(e)}")
         return []
 
-    results = [to_dict(hit["_source"]) for hit in response["hits"]["hits"]]
-    return results
-
+def get_awsauth(region, service):
+    cred = boto3.Session().get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
