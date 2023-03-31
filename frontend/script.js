97f386724fa1cf1ba4b0006e5626022ad30bba08
WeiyaoLi <nolanwyl@gmail.com>
2023-03-30 23:22:09 -0400

    Finished

--- frontend/script.js
+++ frontend/script.js
@@ -15,35 +15,23 @@ var notesList = $('ul#notes');
 
 var noteContent = '';
 
-// Get all notes from previous sessions and display them.
 var notes = getAllNotes();
 renderNotes(notes);
 
 
 
-/*-----------------------------
-      Voice Recognition
-------------------------------*/
 
-// If false, the recording will stop after a few seconds of silence.
-// When true, the silence period is longer (about 15 seconds),
-// allowing us to keep recording even when the user pauses.
 recognition.continuous = true;
 
-// This block is called every time the Speech APi captures a line.
 recognition.onresult = function(event) {
 
-    // event is a SpeechRecognitionEvent object.
-    // It holds all the lines we have captured so far.
-    // We only need the current one.
+
     var current = event.resultIndex;
 
-    // Get a transcript of what was said.
+
     var transcript = event.results[current][0].transcript;
 
-    // Add the current transcript to the contents of our Note.
-    // There is a weird bug on mobile, where everything is repeated twice.
-    // There is no official solution so far so we have to handle an edge case.
+
     var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
 
     if(!mobileRepeatBug) {
@@ -68,9 +56,7 @@ recognition.onerror = function(event) {
 
 
 
-/*-----------------------------
-      App buttons and input
-------------------------------*/
+
 
 $('#start-record-btn').on('click', function(e) {
     if (noteContent.length) {
@@ -87,7 +73,7 @@ $('#pause-record-btn').on('click', function(e) {
     instructions.text('Voice recognition paused.');
 });
 
-// Sync the text inside the text area with the noteContent variable.
+
 noteTextarea.on('input', function() {
     noteContent = $(this).val();
 })
@@ -99,11 +85,10 @@ $('#save-note-btn').on('click', function(e) {
         instructions.text('Could not save empty note. Please add a message to your note.');
     }
     else {
-        // Save note to localStorage.
-        // The key is the dateTime with seconds, the value is the content of the note.
+
         saveNote(new Date().toLocaleString(), noteContent);
 
-        // Reset variables and update UI.
+
         noteContent = '';
         renderNotes(getAllNotes());
         noteTextarea.val('');
@@ -117,13 +102,11 @@ notesList.on('click', function(e) {
     e.preventDefault();
     var target = $(e.target);
 
-    // Listen to the selected note.
     if(target.hasClass('listen-note')) {
         var content = target.closest('.note').find('.content').text();
         readOutLoud(content);
     }
 
-    // Delete note.
     if(target.hasClass('delete-note')) {
         var dateTime = target.siblings('.date').text();
         deleteNote(dateTime);
@@ -133,14 +116,9 @@ notesList.on('click', function(e) {
 
 
 
-/*-----------------------------
-      Speech Synthesis
-------------------------------*/
-
 function readOutLoud(message) {
     var speech = new SpeechSynthesisUtterance();
 
-    // Set the text and voice attributes.
     speech.text = message;
     speech.volume = 1;
     speech.rate = 1;
@@ -150,10 +128,6 @@ function readOutLoud(message) {
 }
 
 
-
-/*-----------------------------
-      Helper Functions
-------------------------------*/
 
 function renderNotes(notes) {
     var html = '';
